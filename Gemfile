51e158421130d9ccb9fa35e7a1df0232574e9a6b
Max223871 <julianmaxin007@gmail.com>
2023-10-23 15:27:06 +0200

    added home page contoller and view

--- Gemfile
+++ Gemfile
@@ -44,6 +44,9 @@ gem "bootsnap", require: false
 
 gem 'devise'
 
+gem 'active_scaffold'
+
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -6,6 +7,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "posts#index"
+  # root "posts#index"
   devise_for :users
 end
