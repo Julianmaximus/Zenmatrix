c8abd549f0795ea1e87971626dbf99f9da89a0ef
Max223871 <julianmaxin007@gmail.com>
2023-12-16 03:24:53 +0100

    working on homepage11

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
-    @posts = Post.order(created_at: :desc).limit(6)
+    @posts = Post.order(created_at: :desc).limit(8)
   end
 
   def about
